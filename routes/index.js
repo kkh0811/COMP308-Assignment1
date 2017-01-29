8748defe88df9054e7db3c742fae21a360dc7f82
kkh0811 <gkim16@my.centennialcollege.ca>
2017-01-29 13:18:33 -0500

    added bower, bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
 });
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About' });
+});
+
 
 module.exports = router;
