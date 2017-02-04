c57771dd5f8233a9fc57e80834ab8ae390633994
kkh0811 <gkim16@my.centennialcollege.ca>
2017-02-04 10:46:00 -0500

    added pages with css

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,19 @@ router.get('/about', (req, res, next) => {
   res.render('about', { title: 'About' });
 });
 
+/* GET project page. */
+router.get('/project', (req, res, next) => {
+  res.render('project', { title: 'project' });
+});
+
+/* GET service page. */
+router.get('/service', (req, res, next) => {
+  res.render('service', { title: 'service' });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'contact' });
+});
 
 module.exports = router;
